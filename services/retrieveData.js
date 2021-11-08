94a7365d189062e3974a77c5a172e05a331a1bec
WAHYU FATUR RIZKI <wahyu.faturrisky@innovez-one.com>
2021-11-08 18:31:01 +0700

    fix: add mock data because issue cors

--- services/retrieveData.js
+++ services/retrieveData.js
@@ -4,7 +4,15 @@ import { RequestPublic } from './headerRequest';
  * @return { obj }
  */
 
+// FIXME: CORS issue in BE side from moduit API, please take a look thankyou, while now I'm adding mockdata first
 export const getQuestionOneFrontEnd = () => {
 	const response = RequestPublic().get('/frontend/web/question/one');
+	return response;
+};
+
+export const getQuestionTwoFrontEnd = () => {
+	const response = RequestPublic().get(
+		'/frontend/web/question//frontend/web/question/two'
+	);
 	return response;
 };
